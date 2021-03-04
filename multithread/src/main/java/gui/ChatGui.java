1da43ed2e29f42a1044c07284cadec7c82da45a3
Tommy <tommy.poupineau@gmail.com>
2021-03-04 12:14:08 +0100

    GUI en cours

--- multithread/src/main/java/gui/ChatGui.java
+++ multithread/src/main/java/gui/ChatGui.java
@@ -31,8 +31,6 @@ public class ChatGui extends JFrame {
 
     public ChatGui(BotThread bot) {
 
-        // final GridBagConstraints gc = new GridBagConstraints();
-
         this.bot = bot;
         this.gc.gridx = 1;
 
@@ -41,49 +39,56 @@ public class ChatGui extends JFrame {
         this.textFieldSubmit = new JTextField(20);
         getJPanelChat().setBackground(Color.cyan);
         this.JPanelChat.add(textFieldSubmit);
-        this.JPanelChat.add(new JButton(new AbstractAction("Envoyer") {
+
+
+        AbstractAction abstractActionButton = new AbstractAction("Envoyer") {
             private static final long serialVersionUID = 1L;
 
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        addStringToGUI(textFieldSubmit.getText());
-                    }
-                });
-            }
-        }), this.gc);
+                if (!textFieldSubmit.getText().equals("")) {
 
+                    String composant = e.getActionCommand();
+                    System.out.println("Action sur le composant : " + composant);
+
+                    if (e.getSource() == textFieldSubmit || e.getSource() == e || composant.equals("Envoyer")) {
+                        SwingUtilities.invokeLater(() -> addStringToGUI(textFieldSubmit.getText()));
+
+
+                    }
+                }
+            }
+        };
+
+        JButton buttonSubmit = new JButton(abstractActionButton);
+
+        textFieldSubmit.addActionListener(abstractActionButton);
+
+        this.JPanelChat.add(buttonSubmit, gc);
 
 
         JPanelChat.setVisible(true);
     }
 
-    public void addStringToGUI(String input)
-    {
+    public void addStringToGUI(String input) {
         JPanelChat.add(new JLabel(input), this.gc);
         this.gc.gridx = 3;
         JPanelChat.validate();
         JPanelChat.repaint();
-        synchronized(bot)
-        {
+        synchronized (bot) {
             this.bot.notify();
             this.bot.setInput(input);
         }
+        textFieldSubmit.setText("");
     }
 
 
     public void envoyerMessage(String message) {
 
 
-
-
         System.out.println("envoyé");
     }
-
-
 
 
     public static ChatGui getFenetrePrincipale() {
